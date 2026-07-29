06f81ce1c75cdfd8e2e94a8f25f84016597a67a1
kshreya-28 <chittoriakshreya28@gmail.com>
2026-07-29 14:31:07 +0530

    Time: 0 ms (100%), Space: 7.7 MB (65.53%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        if(n==0){return 0;}
+        
+        int a=0;
+        int b=1;
+        
+        for(int i=1; i<n;i++){
+            int next=a+b;
+
+            a=b;
+            b=next;
+        }
+        return b;
+    }
+};
