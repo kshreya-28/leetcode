131c678181755bba19ec3ee39b5cdfaf1d4f9829
kshreya-28 <chittoriakshreya28@gmail.com>
2026-08-29 21:12:50 +0530

    Time: 0 ms (100%), Space: 9.5 MB (92.35%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> a(26,0);
+        if(s.size()!=t.size()){return false;}
+        for(int i=0;i<s.size();i++){
+            a[s[i]-'a']++;
+            a[t[i]-'a']--;
+        }
+ 
+         for(int i=0;i<26;i++){
+            if(a[i]!=0){return false;}
+         }
+         return true;
+        
+    }
+};
