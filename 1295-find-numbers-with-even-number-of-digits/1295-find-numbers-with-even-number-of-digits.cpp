c851bf4715a4edbd532c863560469e38753979df
kshreya-28 <chittoriakshreya28@gmail.com>
2026-09-06 21:19:04 +0530

    Time: 0 ms (100%), Space: 13.6 MB (19.45%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -5,6 +5,6 @@ public:
         for(int i=0;i<nums.size();i++){
             if( (nums[i]>9 && nums[i]<100) ||  (nums[i]>999 && nums[i]<10000) || (nums[i]==100000)){cnt++;}
         }
-        return cnt++;
+        return cnt;
     }
 };
